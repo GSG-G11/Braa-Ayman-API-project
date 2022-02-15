1f33107111eefaca41471f7dfc72a594f72a453b
AFahgan <aymanfahgan@gmail.com>
2022-02-15 22:04:34 +0200

    update files

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ window.onload = () => {
 };
 
 submitBtn.addEventListener('click', ()=>{
-    aResult.innerHTML='';
+   
     const animeName = input.value;
     const mainUrl= `https://kitsu.io/api/edge/anime?filter%5Btext%5D=${animeName}` // posterImage.original & attributes.titles.en &synopsis
    const galaryUrl= `https://api.giphy.com/v1/gifs/search?api_key=BEhmVfKdPW8J3qZKPhXMCyNszDlbEci4&q=${animeName}` //images
@@ -49,9 +49,19 @@ submitBtn.addEventListener('click', ()=>{
   atitleDiv.classList.add('anime-name');
   aCard.appendChild(atitleDiv);
 
+ 
   const atitle = document.createElement('h3');
-  atitle.textContent = e.attributes.titles.en
+  atitle.textContent = e.attributes.titles.en_jp
   atitleDiv.appendChild(atitle);
+
+  const asynopsisDiv = document.createElement('div');
+  asynopsisDiv.classList.add('synopsis');
+  aCard.appendChild(asynopsisDiv);
+
+ 
+  const asynopsis = document.createElement('blockquote');
+  asynopsis.textContent = e.attributes.synopsis
+  asynopsisDiv.appendChild(asynopsis);
 
   const abuttDiv = document.createElement('div');
   abuttDiv.classList.add('anime-btn');
