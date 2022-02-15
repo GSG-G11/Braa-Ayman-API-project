d0601a179b5909f3c32449ec66c2b14a0084bbac
braaAwni <braaberoo95@gmail.com>
2022-02-15 14:04:58 +0200

    add function fetch To bring XML

---
+++ main.js
@@ -0,0 +1,13 @@
+const fetch =(method,url,cb) =>{
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState===4){
+            if(xhr.status===200){
+                cb(JSON.parse(xhr.responseText))
+            }
+        }
+    }
+    xhr.open(method,url);
+    xhr.send();
+}
+
